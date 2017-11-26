20a32bbae338ce6ffa462e1cc749a5bf302d204b
ghdisgh <dlduswn@2351>
2017-11-26 17:55:29 +0900

    Version 1.1

--- app/src/main/java/com/example/iyeonju/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/iyeonju/myapplication/Main2Activity.java
@@ -1,85 +1,37 @@
 package com.example.iyeonju.myapplication;
 
-import android.support.annotation.IdRes;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.content.Intent;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 
+public class Main2Activity extends FragmentPagerAdapter {
 
-public class Main2Activity extends AppCompatActivity {
+    int tabCount;
 
-    RadioGroup radioGroup;
-    RadioButton radioButton;
-    RadioButton radioButton2;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        Log.i("DEBUG", "Hello at the Main2Activity");
-
-        radioGroup = (RadioGroup) findViewById(R.id.radioGroup) ;
-        radioButton = (RadioButton) findViewById(R.id.radioButton);
-        radioButton2 = (RadioButton) findViewById(R.id.radioButton2);
-        radioGroup.setOnCheckedChangeListener(radioGroupButtonChangeListener);
-
+    public Main2Activity(FragmentManager fm, int numberOfTabs) {
+        super(fm);
+        this.tabCount = numberOfTabs;
     }
 
-    RadioGroup.OnCheckedChangeListener radioGroupButtonChangeListener = new RadioGroup.OnCheckedChangeListener(){
-
-        @Override
-        public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
-            if (i==R.id.radioButton2){
-                Toast.makeText(Main2Activity.this, "여자", Toast.LENGTH_SHORT).show();
-            }
-            else if (i==R.id.radioButton){
-                Toast.makeText(Main2Activity.this, "남자", Toast.LENGTH_SHORT).show();
-            }
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                BlankFragment tab1 = new BlankFragment();
+                return tab1;
+            case 1:
+                BlankFragment2 tab2 = new BlankFragment2();
+                return tab2;
+            case 2:
+                BlankFragment3 tab3 = new BlankFragment3();
+                return tab3;
+            default:
+                return null;
         }
-    };
-
-    protected void onPause() {
-        super.onPause();
-        Log.i("TAG", "HYWU on Pause");
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i("TAG", "HYWU on Create");
+    public int getCount() {
+        return tabCount;
     }
-
-
-    public void NextScreen2(View view){
-
-        Intent i = new Intent (this, MainActivity.class);
-        startActivity(i);
-
-        Toast toast= Toast.makeText(getApplicationContext(), "hello2 ", Toast.LENGTH_LONG ) ;
-        toast.show();
-
-    }
-
-    public void copy(View view){
-        EditText aa = (EditText) findViewById(R.id.editText7);
-        aa.getText();
-
-        TextView bb = (TextView) findViewById(R.id.editText5);
-        bb.getText();
-
-        TextView cc = (TextView) findViewById(R.id.editText2);
-        cc.getText();
-
-        TextView dd = (TextView) findViewById(R.id.editText3);
-        dd.getText();
-
-    }
-
 }
--- app/src/main/java/com/example/iyeonju/myapplication/MainActivity.java
+++ app/src/main/java/com/example/iyeonju/myapplication/MainActivity.java
@@ -1,107 +1,90 @@
 package com.example.iyeonju.myapplication;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TabHost;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends AppCompatActivity
-{
-    EditText etheight,etweight, etm;
-    TextView bmires, pmres;
-    TabHost tabHost;
+public class MainActivity extends AppCompatActivity {
+
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    RadioButton radioButton2;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("각종 계산기");
-        tabHost = (TabHost)findViewById(R.id.tabhost1);
-        tabHost.setup();
 
-        TabHost.TabSpec tab1 = tabHost.newTabSpec("A").setContent(R.id.tab1).setIndicator("회원 가입");
-        tabHost.addTab(tab1);//1번탭 생성
-        TabHost.TabSpec tab2 = tabHost.newTabSpec("B").setContent(R.id.tab2).setIndicator("BMI계산기");
-        tabHost.addTab(tab2);//2번탭 생성
-        TabHost.TabSpec tab3 = tabHost.newTabSpec("C").setContent(R.id.tab3).setIndicator("소개");
-        tabHost.addTab(tab3);//3번탭 생성
+        TabLayout tabLayout =
+                (TabLayout) findViewById(R.id.tab_layout);
 
-        etheight = (EditText)findViewById(R.id.etheight);
-        etweight = (EditText)findViewById(R.id.etweight);
-        bmires = (TextView)findViewById(R.id.tvbmires);
-        pmres = (TextView)findViewById(R.id.tvmetres);
+        tabLayout.addTab(tabLayout.newTab().setText("로그인"));
+        tabLayout.addTab(tabLayout.newTab().setText("회원가입"));
+        tabLayout.addTab(tabLayout.newTab().setText("나의 소개"));
+
+        final ViewPager viewPager =
+                (ViewPager) findViewById(R.id.pager);
+
+        final PagerAdapter adapter = new Main2Activity
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new
+                TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup) ;
+        radioButton = (RadioButton) findViewById(R.id.radioButton);
+        radioButton2 = (RadioButton) findViewById(R.id.radioButton2);
+
     }
 
-    public void NextScreen1(View view){
+    public void NextScreen3(View view){
 
-        Intent i = new Intent (this, Main2Activity.class);
+        Intent i = new Intent (this, MainActivity.class);
         startActivity(i);
 
-        Toast toast= Toast.makeText(getApplicationContext(), "hello2 ", Toast.LENGTH_LONG ) ;
+        Toast toast= Toast.makeText(getApplicationContext(), "메인페이지", Toast.LENGTH_LONG ) ;
         toast.show();
 
     }
 
-    //BMI 버튼 리스너
-    public void bmilistener(View v)
-    {
-        if (etheight.getText().toString().replace(" ","").equals("") || etweight.getText().toString().replace(" ","").equals(""))
-        {
-            checkemptybmi();
-        }
-        else
-        {
-            if(v.getId() == R.id.bmibutton)
-            {
-                double h = Double.parseDouble(etheight.getText().toString());
-                double w = Double.parseDouble(etweight.getText().toString());
-                double bmi = w / ((h/100) * (h/100));
-                if (bmi < 18.5)
-                {
-                    bmires.setText("저체중이에요~");
-                }
-                else if (bmi >= 18.5 && bmi < 23)
-                {
-                    bmires.setText("정상입니다! ^ㅅ^");
-                }
-                else if (bmi >= 23 && bmi < 25)
-                {
-                    bmires.setText("과체중이에요..;ㅁ;");
-                }
-                else
-                {
-                    bmires.setText("비만입니다!!! :-(");
-                }
-            }
-        }
+    public void copy(View view){
+        EditText aa = (EditText) findViewById(R.id.editText7);
+        aa.getText();
+
+        TextView bb = (TextView) findViewById(R.id.editText5);
+        bb.getText();
+
+        TextView cc = (TextView) findViewById(R.id.editText2);
+        cc.getText();
+
+        TextView dd = (TextView) findViewById(R.id.editText3);
+        dd.getText();
+
     }
-
-    public void checkemptybmi()
-    {
-        if(etheight.getText().toString().replace(" ","").equals(""))
-        {
-            Toast.makeText(getApplicationContext(), "값을 입력하세요", Toast.LENGTH_SHORT).show();
-            etheight.requestFocus();
-        }
-        else if (etweight.getText().toString().replace(" ","").equals(""))
-        {
-            Toast.makeText(getApplicationContext(), "값을 입력하세요", Toast.LENGTH_SHORT).show();
-            etweight.requestFocus();
-        }
-        else if (etweight.getText().toString().replace(" ","").equals("") && etheight.getText().toString().replace(" ","").equals(""))
-        {
-            Toast.makeText(getApplicationContext(), "값을 입력하세요", Toast.LENGTH_SHORT).show();
-            etheight.requestFocus();
-        }
-    }
-
-
 }
